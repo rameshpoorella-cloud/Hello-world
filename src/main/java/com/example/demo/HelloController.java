1794b6bd99b8f3445d7972c01e2d8dfad71390d3
Ramesh Poorella <ramesh.poorella@gmail.com>
2026-03-22 17:41:40 +0400

    Initial commit

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello, Spring Boot!";
+    }
+
+    @GetMapping("/about")
+    public String about() {
+        return "This is a simple Spring Boot project.";
+    }
+}
